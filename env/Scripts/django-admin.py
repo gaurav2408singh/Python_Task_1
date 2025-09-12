7f5a4c85998e95915df4a7f3c8b8b82651eb5101
Yash0951 <yash951official@gmail.com>
2025-09-12 17:24:48 +0530

    FIX: Remove env, database, and pycache from tracking

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\06_my projects\FOSSIE internship\Task 1\workshop_booking\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
